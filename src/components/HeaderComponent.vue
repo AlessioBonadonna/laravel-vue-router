83058374a3b9d57ddd3181a0c96533ff314ab9c5
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-18 16:17:36 +0100

    first comit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Il Mio Portfolio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="(item, index) in menuNav" :key="index">
+                <router-link :to="{name : item.routeName}" class="nav-link">
+            {{ item.label }}
+            </router-link>
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data(){
+            return {
+                menuNav: [
+                    {
+                        label: 'Home',
+                        routeName: 'home'
+                    },
+                    {
+                        label: 'Projects',
+                        routeName: 'projects'
+                    },
+                    
+                ]
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+sono la homepage
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HomePage',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
